301ec6cc5675ffc36a2a3969f191eea0f21c73be
Bjoern Siepe <bjoernsiepe@gmail.com>
2023-02-28 19:19:46 +0100

    redo typing example

--- figures-dynamische-netzwerke-workshop.R
+++ figures-dynamische-netzwerke-workshop.R
@@ -3,13 +3,13 @@ library(qgraph)
 
 node_names <- c("Geschwin-\ndigkeit", "Fehler")
 
-bw_mat <- matrix(c(0,.9,0,0), nrow = 2)
-wi_mat <- matrix(c(0,0,-.9,0), nrow = 2)
+wi_mat <- matrix(c(0,.9,0,0), nrow = 2)
+bw_mat <- matrix(c(0,0,-.9,0), nrow = 2)
 
 svg("typingspeed.svg")
 par(mfrow = c(1,2))
 qgraph(bw_mat, theme = "colorblind", layout = "spring", labels = node_names,
-       vsize = 15, esize = 8, directed = FALSE, title = "Zwischen")
+       vsize = 15, esize = 8, directed = FALSE, title = "Zwischen", negDashed = TRUE)
 
 qgraph(wi_mat, theme = "colorblind", layout = "spring", labels = node_names,
        vsize = 15, esize = 8, directed = FALSE, title = "Innerhalb")
